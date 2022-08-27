1f184f867dca5c1a8645d1ea99d846a7211f299b
NakamuraTakuto <rulitaku@icloud.com>
2022-08-27 17:18:07 +0900

    Playerに合わせて子Objectを動かす

--- Assets/Sclipt/PlayerContller.cs
+++ Assets/Sclipt/PlayerContller.cs
@@ -6,15 +6,17 @@ public class PlayerContller : MonoBehaviour
 {
     /// <summary>Playerの移動速度</summary>
     [Header("Playerの移動速度"),SerializeField] float _moveSpeed = 5f;
-    Rigidbody2D _rd;
-    Animator _anima;
     /// <summary>Playerのアイテムリスト</summary>
     [Header("Playerのアイテムリスト"), SerializeField]
     List<ObjectBase> _itemuList = new List<ObjectBase>();
+    Rigidbody2D _rd;
+    Animator _anima;
+    Transform _rotation;
     // Start is called before the first frame update
     void Start()
     {
         _rd = GetComponent<Rigidbody2D>();
+        _rotation =transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -25,15 +27,32 @@ public class PlayerContller : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
         Vector2 dir = new Vector2(h, v).normalized;
         _rd.velocity = dir * _moveSpeed;
+
+        //Playerの向きに合わせて子Objectの向きを調整する
+        if (Mathf.Abs(h) > 0)
+        {
+            //Playerの向いている方向に合わせて動く
+            _rotation.localEulerAngles = new Vector3(0, 0, h * -90);
+            
+        }
+        else if (Mathf.Abs(v) > 0)
+        {
+            //Playerの向いている方向に動く
+            _rotation.localEulerAngles = new Vector3(0, 0, v > 0 ? 0 : 180);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Object")
         {
-            Debug.Log("当たった");
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //Triggerに接触しているObjectをActiveにする
+            }
         }
     }
+
 
     public void ItemGet(ObjectBase item)
     {
